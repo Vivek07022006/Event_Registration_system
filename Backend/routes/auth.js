7aa457fcb36f2baa7b4601fb03a6759c72950dde
Vivek S <147020432+Vivek07022006@users.noreply.github.com>
2024-12-13 23:12:52 +0530

    Update auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -36,7 +36,7 @@ router.post('/login', async (req, res) => {
     }
 
     const user = await User.findOne({ email });
-    if (!user || !(await user.comparePassword(password))) {
+    if (!user || !(password) ) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
